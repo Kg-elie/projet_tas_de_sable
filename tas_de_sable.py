dfe062a618e01c8c576cd1204db65018b0f27f95
iram MADANI FOUATIH <iram.madanifouatih@uvsq.fr>
2022-02-15 11:06:40 +0100

    test

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -3,6 +3,7 @@
 # groupe BI TD 1
 # Elie KANGA
 # Sarah Medeneche
-# Iram MADANI FOUATHI
+# Iram MADANI FOUATIH
 #  https://github.com/uvsq22101259/projet_tas_de_sable
 #########################################
+print("HI")
