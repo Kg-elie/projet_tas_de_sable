96949c0a323ac21a1c07e3ea6271e18ab6629d2f
elie KANGA <elie.kanga@ens.uvsq.fr>
2022-02-15 19:30:32 +0100

    creation fonction (configuration et placements des valeurs)

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -10,33 +10,62 @@
 # importation librairie                 #
 #########################################
 import tkinter as tk
+import random as rd
+
 
 #########################################
 # definitions des constantes            #
 #########################################
-HEIGHT = 600
-WIDTH = 600
+HEIGHT = 1000
+WIDTH = 1000
 
 #########################################
 #  definitions des variables            #
 #########################################
-
-
+ligne = 100
+l = []
 #########################################
 # definitions des fonctions             #
 #########################################
 def grillage(n,taille):
-    """ cree une grille de n^n case"""
+    """ cree une grille de n^2 case"""
     rythme = taille // n
     x = rythme
     y = rythme 
     
     for i in range (n):
-        canvas.create_line((0,y),(600,y), fill= "white")
+        canvas.create_line((1,y),(taille,y), fill= "white")
     
-        canvas.create_line((x,0),(x,600), fill= "white")
+        canvas.create_line((x,1),(x,taille), fill= "white")
         x += rythme
         y += rythme
+       
+
+
+def configuration(n):
+    """remplie la grille d'une configuration aléatoire"""
+    global l 
+    for i in range(n):
+        a = []
+        for j in range(n):
+            a.append(rd.randint(0,5))
+        l.append(list(a))
+    for i in l:
+        print(i)
+        
+
+def  placement(n,taille):
+    rythme = taille // n
+    x = 0
+    y = 0
+    
+    for i in l:
+        x = 0
+        for j in i:
+            canvas.create_text((x+(rythme//2),y+(rythme//2)), text= j,fill="white")
+            x += rythme
+        y+= rythme
+
 
 #########################################
 # programme principal
@@ -44,9 +73,12 @@ racine = tk.Tk()
 
 canvas = tk.Canvas(racine,width= WIDTH, height= HEIGHT, bg= "black")
 canvas.grid(column=0,row=0)
-grillage(60,600)
+grillage(ligne,1000)
+configuration(ligne)
+placement(ligne,1000)
 
 bouton = tk.Button(racine,text="bouton")
+
 bouton.grid(column=0,row=1)
 
 
