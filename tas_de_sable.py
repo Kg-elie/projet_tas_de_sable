d632d44cb47f0a6556a441aaef5028a224220c9c
elie KANGA <elie.kanga@ens.uvsq.fr>
2022-02-15 10:30:18 +0100

    premier fichiers

---
+++ tas_de_sable.py
@@ -0,0 +1,8 @@
+
+#########################################
+# groupe BI TD 1
+# Elie KANGA
+# Sarah Medeneche
+# Iram MADANI FOUATHI
+#  https://github.com/uvsq22101259/projet_tas_de_sable
+#########################################
