46ef11d687007252c881e97ba21605840a695e6a
elie kanga <elie.kanga@ens.uvsq.fr>
2022-02-20 19:55:28 +0100

    couleurs associées aux chiffres

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -23,26 +23,42 @@ WIDTH = 600
 #########################################
 #  definitions des variables            #
 #########################################
-ligne = 50
+ligne = 35
 l = []
+case_id = []
 #########################################
 # definitions des fonctions             #
 #########################################
 def grillage(n,taille):
+    global case_id
     """ cree une grille de n^2 case"""
+    if len(case_id) > 0:
+        case_id = []
+    
     rythme = taille // n
     x = 0
     y = 0
-    colors = ["black", "red","blue"]
     
     for i in range (n):
         x = 0
         for j in range(n):
-            couleur = rd.choice(colors)
-            canvas.create_rectangle((x,y),(x+rythme,y+rythme), fill=couleur ,outline=couleur )
+            case_id.append(canvas.create_rectangle((x,y),(x+rythme,y+rythme), fill="black" ,outline="white", width= 2 ))
             x += rythme
         y += rythme
-       
+    id = 0
+    
+    for i in l:
+        for j in i:
+            if j == 0:
+                canvas.itemconfig(case_id[id], fill ="blue")
+            elif j == 1:
+                canvas.itemconfig(case_id[id], fill="red")
+            elif j == 2:
+                canvas.itemconfig(case_id[id], fill="green")
+            elif j == 3:
+                canvas.itemconfig(case_id[id], fill="purple")
+            id += 1
+    
 
 
 def configuration(n):
@@ -74,10 +90,27 @@ def  placement(n,taille):
 def construction_terrain(n,taille):
     """ construire le terrain à partir de plusieus fonctions"""
     
-    grillage(n,taille)
+
     configuration(n)
+    grillage(n,taille)
     placement(n,taille)
+
     
+def change_couleurs():
+    global case_id
+    id = 0
+    
+    for i in l:
+        for j in i:
+            if j == 0:
+                canvas.itemconfig(case_id[id], fill ="blue")
+            elif j == 1:
+                canvas.itemconfig(case_id[id], fill="red")
+            elif j == 2:
+                canvas.itemconfig(case_id[id], fill="green")
+            elif j == 3:
+                canvas.itemconfig(case_id[id], fill="purple")
+            id += 1
     
 #########################################
 # programme principal
@@ -87,8 +120,12 @@ canvas = tk.Canvas(racine,width= WIDTH, height= HEIGHT, bg= "black")
 canvas.grid(column=0,row=0)
 
 
-bouton = tk.Button(racine,text="configuration aléatoire",command=  partial( construction_terrain,ligne,HEIGHT))
+bouton = tk.Button(racine,text="configuration aléatoire",command=  lambda : construction_terrain(ligne,HEIGHT))
 bouton.grid(column=0,row=1)
+
+bouton = tk.Button(racine,text="couleurs",command= change_couleurs)
+bouton.grid()
 
 
 racine.mainloop()
+
