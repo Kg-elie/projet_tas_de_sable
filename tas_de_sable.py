04b0ab5dbd923d7a9ea31f56475943575ba0f0e5
elie kanga <elie.kanga@ens.uvsq.fr>
2022-03-01 12:58:39 +0100

    ajout gitignore et fonction sauvegarde

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -3,13 +3,13 @@
 # Elie KANGA
 # Sarah Medeneche
 # Iram MADANI FOUATIH
-#  https://github.com/uvsq22101259/projet_tas_de_sable
+# https://github.com/uvsq22101259/projet_tas_de_sable
 #########################################
 
 #########################################
 # importation librairie                 #
 #########################################
-from functools import partial
+
 import tkinter as tk
 import random as rd
 
@@ -23,26 +23,31 @@ WIDTH = 600
 #########################################
 #  definitions des variables            #
 #########################################
-ligne = 100
+# coisir une ligne qui ne fit pas de decimale si l'on divise la taille par elle
+ligne =4
+# matrice contenant les grain de sable pour chaque case
 l = []
+# liste contenant les identifiants de chaque case
 case_id = []
 #########################################
 # definitions des fonctions             #
 #########################################
 def grillage(n,taille):
     global case_id
-    """ cree une grille de n^2 case"""
+    """ cree une grille de n^2 case, et donne a chaque case une couleur selectionner en fonction du grain de sable qu'elle contient """
     if len(case_id) > 0:
+        for i in case_id:
+            canvas.delete(i)
         case_id = []
     
     rythme = taille // n
-    x = 2
-    y = 2
+    x = 0
+    y = 3
     
     for i in range (n):
-        x = 2
+        x = 3
         for j in range(n):
-            case_id.append(canvas.create_rectangle((x,y),(x+rythme,y+rythme), fill="black" ,outline="white", width= 2 ))
+            case_id.append(canvas.create_rectangle((x,y),(x+rythme,y+rythme), fill="black" ,outline="black", width= 2 ))
             x += rythme
         y += rythme
     id = 0
@@ -50,16 +55,17 @@ def grillage(n,taille):
     for i in l:
         for j in i:
             if j == 0:
-                canvas.itemconfig(case_id[id], fill ="blue")
+                canvas.itemconfig(case_id[id], fill ="grey")
             elif j == 1:
-                canvas.itemconfig(case_id[id], fill="red")
-            elif j == 2:
-                canvas.itemconfig(case_id[id], fill="green")
-            elif j == 3:
                 canvas.itemconfig(case_id[id], fill="purple")
+            elif j == 2:
+                canvas.itemconfig(case_id[id], fill="blue")
+            elif j == 3:
+                canvas.itemconfig(case_id[id], fill="cyan")
+            elif j >= 4:
+                canvas.itemconfig(case_id[id], fill="yellow")
             id += 1
     
-
 
 def configuration(n):
     """remplie la grille d'une configuration aléatoire"""
@@ -68,24 +74,24 @@ def configuration(n):
     for i in range(n):
         a = []
         for j in range(n):
-            a.append(rd.randint(0,3))
+            a.append(rd.randint(0,4))
         l.append(list(a))
-    
-    
-        
+    print(l)
+          
 
 def  placement(n,taille):
-    """place les nombre de grain de sables sur les widgets"""
+    """place les nombres de grain de sables sur les widgets"""
     rythme = taille // n
     x = 0
-    y = 0
+    y = 3
     
     for i in l:
-        x = 0
+        x = 3
         for j in i:
             canvas.create_text((x+(rythme//2),y+(rythme//2)), text= j,fill="white")
             x += rythme
         y+= rythme
+
 
 def construction_terrain(n,taille):
     """ construire le terrain à partir de plusieus fonctions"""
@@ -93,39 +99,100 @@ def construction_terrain(n,taille):
 
     configuration(n)
     grillage(n,taille)
-    #placement(n,taille)
+    placement(n,taille)
 
     
-def change_couleurs():
-    global case_id
-    id = 0
+def ecoulement(n,taille):
+    """simule un ecoulement en donnant a chaque case voisine un grain de sabke si la case est surchargée"""
+    global case_id, l
+    for i in range(len(l)):
+        for j in range(len(l[i])):
+            if l[i][j]>= 4 and (j > 0) and (i > 0) and (j < n-1) and (i < n-1):
+                l[i][j] -= 4 
+                l[i-1][j] +=1 
+                l[i+1][j] += 1
+                l[i][j-1] += 1
+                l[i][j+1] += 1
+            elif l[i][j]>= 4 and (j == 0) and (i == 0):
+                l[i][j] -= 4 
+                l[i+1][j] += 1
+                l[i][j+1] += 1
+            elif l[i][j]>= 4 and  j == n-1 and i == n-1:
+                l[i][j] -= 2 
+                l[i-1][j] +=1 
+                l[i][j-1] += 1
+            elif l[i][j]>= 4 and j == 0 and i > 0  and i < n-1:
+                l[i][j] -= 4 
+                l[i-1][j] +=1 
+                l[i+1][j] += 1
+                l[i][j+1] += 1
+            elif l[i][j]>= 4 and j == 0 and i == n-1:
+                l[i][j] -= 4 
+                l[i-1][j] +=1 
+                l[i][j+1] += 1
+            elif l[i][j]>= 4 and i > 0 and j == n-1 and i < n-1:
+                l[i][j] -= 4 
+                l[i-1][j] +=1 
+                l[i][j-1] += 1
+                l[i+1][j] += 1
+            elif l[i][j]>= 4 and j > 0 and j < n-1 and i == n-1:
+                l[i][j] -= 4 
+                l[i-1][j] +=1 
+                l[i][j-1] += 1
+                l[i][j+1] += 1
+            elif l[i][j]>= 4 and j > 0 and i == 0 and j < n-1 :
+                l[i][j] -= 4 
+                l[i+1][j] += 1
+                l[i][j-1] += 1
+                l[i][j+1] += 1
+            elif l[i][j]>= 4 and j == n-1 and i == 0:
+                l[i][j] -= 4 
+                l[i+1][j] +=1 
+                l[i][j-1] += 1
+    grillage(n,taille)
+    #placement(n,taille)
+    racine.after(500,lambda : ecoulement(n,taille))
+                
+                        
+def configuration_geometrique(n):
+    """cree une configuration ou la case du centre est surcharger a l'infini et ne fait que donner des grains"""
+    global l 
+    l = []
+    for i in range(n):
+        a = []
+        for j in range(n):
+            if i == n//2 and j == n//2:
+                a.append(1300000)
+            else:
+                a.append(0) 
+        l.append(list(a))
+
+
+def copie():
+    """copie la matrice d'une configuration dans un fichier text"""
+    fic = open("sauvegarde.txt","w")
+    fic.write(str(len(l)) + "\n" + "_____" )
+    fic.close
     
-    for i in l:
-        for j in i:
-            if j == 0:
-                canvas.itemconfig(case_id[id], fill ="blue")
-            elif j == 1:
-                canvas.itemconfig(case_id[id], fill="red")
-            elif j == 2:
-                canvas.itemconfig(case_id[id], fill="green")
-            elif j == 3:
-                canvas.itemconfig(case_id[id], fill="purple")
-            id += 1
     
 #########################################
 # programme principal
 racine = tk.Tk()
 
 canvas = tk.Canvas(racine,width= WIDTH, height= HEIGHT, bg= "black")
-canvas.grid(column=0,row=0)
+canvas.grid(column=1,row=0, rowspan= 20)
 
 
 bouton = tk.Button(racine,text="configuration aléatoire",command=  lambda : construction_terrain(ligne,HEIGHT))
-bouton.grid(column=0,row=1)
+bouton.grid(column=0,row=0)
 
-bouton = tk.Button(racine,text="couleurs",command= change_couleurs)
-bouton.grid()
+bouton1 = tk.Button(racine,text="ecoulement",command=  lambda : ecoulement(ligne,HEIGHT))
+bouton1.grid(column=0,row=1)
 
+bouton2 = tk.Button(racine, text="sauvegarder",command= copie)
+bouton2.grid(column= 0, row= 2 )
 
 racine.mainloop()
+#########################@
+# fin du code
 
